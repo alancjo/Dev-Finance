ceb960b786b268075123cdff8dbe1ae704897ccd
Alan Oliveira <alancjobm@gmail.com>
2021-01-28 20:12:00 -0300

    session 2

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const Modal = {
         document
             .querySelector('.modal-overlay')
             .classList
-            .add('active')
+            .add('active');
     },
     close() { 
         // fechar o modal 
@@ -13,58 +13,83 @@ const Modal = {
         document 
             .querySelector('.modal-overlay')
             .classList
-            .remove('active')    
+            .remove('active') ;   
     }
 }
 
 const transactions = [
     {
         id: 1, 
-        description = 'Luz', 
+        description: 'Luz', 
         amount: -50000, // o valor real é R$ 500,00
         date: '23/01/2021',
     },
     {
         id: 2, 
-        description = 'Website', 
+        description: 'Website', 
         amount: 500000,
         date: '15/01/2021',
     },
     {
         id: 3, 
-        description = 'Internet', 
+        description: 'Internet', 
         amount: -36250,
+        date: '12/01/2021',
+    },
+    {
+        id: 4, 
+        description: 'App', 
+        amount: 600,
         date: '12/01/2021',
     }
 ]
 
 const Transaction = {
-    incomes() {
-        // somar as entradas
-    }, 
-    expenses() {
-        // somar as saídas 
-    }, 
-    total() {
-        // entradas - saídas
-    }
+//     incomes() {
+//         // somar as entradas
+//     }, 
+//     expenses() {
+//         // somar as saídas 
+//     }, 
+//     total() {
+//         // entradas - saídas
+//     }
 }
 
 const DOM = {
-    innerHTMLTransaction() {
+    transactionsContainer: document.querySelector('#data-table'),
 
-        transactions.forEach(element => {
-            console.log(element);
-        });
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction); 
+
+        DOM.transactionsContainer.appendChild(tr)
+    },    
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense";
+
+        const amount = Utils.formatCurrency(transaction.amount);
+
         const html = `
-        <tr>
-            <td class="description">Luz</td>
-            <td class="expense">- R$ 500,00</td>
-            <td class="date">23/01/2021</td>
-            <td>
-                <img src="./assets/minus.svg" alt="Remover transação">
-            </td>
-        </tr>
+        <td class="description">${transaction.description}</td>
+        <td class=${CSSclass}>${transaction.amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td>
+            <img src="./assets/minus.svg" alt="Remover transação">
+        </td>
         `
+
+        return html;
     }
 }
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : "";
+    }
+}
+
+
+transactions.forEach(element => DOM.addTransaction(element));
+
+
